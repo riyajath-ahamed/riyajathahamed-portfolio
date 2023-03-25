668e1b0099eff57231d371501a93b574af0bc5de
riyajath-ahamed <riyajatha@gmail.com>
2023-03-26 00:15:38 +0530

    changes

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-import { BallCanvas } from "./canvas";
+//import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
-import { technologies } from "../constants";
+//import { technologies } from "../constants";
 
 const Tech = () => {
   return (
