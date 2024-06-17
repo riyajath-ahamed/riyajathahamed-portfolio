111a2c59af174da1779c1ebaa9cf8163b1349c3f
riyajath-ahamed <riyajatha@gmail.com>
2024-06-17 19:12:00 +0530

    Font added

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,11 +29,11 @@ module.exports = {
         'custom-hover-border': '1px 1px 0 #151030, -0.04em -0.04em #ffc700, -0.08em -0.08em #ffc700, -0.12em -0.12em #ffc700, -0.16em -0.16em #ffc700, -0.2em -0.2em #ffc700',
       },
       fontFamily:{
-        curve : ["Sedgwick Ave Display", "cursive"]
+        curve : ["paaniPuri", "cursive"]
       },
       animation: {
         wiggle: 'wiggle 1s ease-in-out infinite',
-        'spin-slow': 'spin 4s linear infinite',
+        'spin-slow': 'spin 4s ease-in-out infinite',
       }
     },
   },
