123efecb3cb78750e81b6026161083ebb392f8ae
riyajath-ahamed <riyajatha@gmail.com>
2024-10-08 21:49:19 +0530

    Minor Fix | RJ

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -57,7 +57,7 @@ export const DATA = {
 
         navbar: true,
       },
-      email: {
+      Email: {
         name: "Send Email",
         url: "mailto:riyajatha@gmail.com",
         icon: Icons.email,
@@ -123,18 +123,5 @@ export const DATA = {
       video:
         "https://pub-83c5db439b40468498f97946200806f7.r2.dev/chat-collect.mp4",
     },
-  ],
-  hackathons: [
-    {
-      title: "Hack Western 5",
-      dates: "November 23rd - 25th, 2018",
-      location: "London, Ontario",
-      description:
-        "Developed a mobile application which delivered bedtime stories to children using augmented reality.",
-      image:
-        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/hack-western.png",
-      mlh: "https://s3.amazonaws.com/logged-assets/trust-badge/2019/mlh-trust-badge-2019-white.svg",
-      links: [],
-    },
-  ],
+  ]
 } as const;
