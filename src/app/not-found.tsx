fe3307460c19bd385da779548141eb0a1ca76f22
riyajath-ahamed <riyajatha@gmail.com>
2025-12-06 21:55:18 +0530

    fix: replace anchor tag with Link component for navigation in Custom404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,4 +1,5 @@
 import { SketchUnderline } from "@/components/blog";
+import Link from "next/link";
 
 export default function Custom404() {
   
@@ -13,19 +14,19 @@ export default function Custom404() {
       {/* Center Content */}
       <div className="relative z-10 text-center text-white">
         <p className="text-sm font-semibold font-serif tracking-widest opacity-80 whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-transparent dark:from-white dark:to-slate-900/10">404</p>
-        <p className="text-sm md:text-base font-serif font-bold opacity-90 whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-transparent dark:from-white dark:to-slate-900/10">SORRY, WE COULDN'T FIND THIS PAGE</p>
-        <a
+        <p className="text-sm md:text-base font-serif font-bold opacity-90 whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-transparent dark:from-white dark:to-slate-900/10">SORRY, WE COULDN&apos;T FIND THIS PAGE</p>
+        <Link
           href="/"
           className="inline-block mt-3 text-3xl font-semibold font-serif relative  whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-transparent dark:from-white dark:to-slate-900/10"
         >
           GO BACK
           <SketchUnderline className="stroke-black dark:stroke-white"  />
-        </a>
+        </Link>
       </div>
 
       {/* Bottom Text */}
-      <p className="absolute bottom-6 text-[10px] md:text-xs text-white opacity-75 tracking-widest text-center w-full">
-        THE PAGE YOU ARE LOOKING FOR DOESN'T EXIST OR AN OTHER ERROR OCCURRED.
+      <p className="absolute bottom-6 text-[10px] md:text-xs text-[#00171f] dark:text-white opacity-75 tracking-widest text-center w-full">
+        THE PAGE YOU ARE LOOKING FOR DOESN&apos;T EXIST OR AN OTHER ERROR OCCURRED.
       </p>
     </div>
   );
